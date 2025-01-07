8bcfe35911c0f11d768ea0e304676657f44348f8
Kaelig Deloumeau-Prigent <kaelig@users.noreply.github.com>
2025-01-07 15:55:09 -0800

    Add stackbit config

---
+++ stackbit.config.ts
@@ -0,0 +1,18 @@
+import { ContentfulContentSource } from '@stackbit/cms-contentful'
+
+export default {
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '22',
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+    }),
+  ],
+  models: {
+    post: { type: 'post', urlPath: '/posts/{slug}' },
+  }
+}
